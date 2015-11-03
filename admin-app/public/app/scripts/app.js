358e184c4a5c399ede122d6be894e104cbdab047
Vagner Dutra <vagnerdutra@outlook.com>
2015-11-03 19:30:28 -0200

    add filter angular do mesos

--- admin-app/public/app/scripts/app.js
+++ admin-app/public/app/scripts/app.js
@@ -27,7 +27,8 @@
         'gridshore.c3js.chart',
         'growlNotifications',
         'formly',
-        'formlyBootstrap'
+        'formlyBootstrap',
+        'mesos.services'
         ])
      .config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
         cfpLoadingBarProvider.latencyThreshold = 5;
@@ -95,6 +96,76 @@
             if(switchValue) {
                 $('body').addClass('box-section');
             }
+        })
+        .filter('truncateMesosID', function() {
+            return function(id) {
+                if (id) {
+                    var truncatedIdParts = id.split('-');
+
+                    if (truncatedIdParts.length > 3) {
+                        return 'â€¦' + truncatedIdParts.splice(3, 3).join('-');
+                    } else {
+                        return id;
+                    }
+                } else {
+                    return '';
+                }
+            };
+        })
+        .filter('truncateMesosState', function() {
+            return function(state) {
+                // Remove the "TASK_" prefix.
+                return state.substring(5);
+            };
+        })
+        .filter('isoDate', function($filter) {
+            return function(date) {
+                var i = parseInt(date, 10);
+                if (_.isNaN(i)) { return '' };
+                return $filter('date')(i, 'yyyy-MM-ddTH:mm:ssZ');
+            };
+        })
+        .filter('relativeDate', function() {
+            return function(date, refDate) {
+                var i = parseInt(date, 10);
+                if (_.isNaN(i)) { return '' };
+                return relativeDate(i, refDate);
+            };
+        })
+        .filter('slice', function() {
+            return function(array, begin, end) {
+                if (_.isArray(array)) {
+                    return array.slice(begin, end);
+                }
+            };
+        })
+        .filter('unixDate', function($filter) {
+            return function(date) {
+                if ((new Date(date)).getFullYear() == (new Date()).getFullYear()) {
+                    return $filter('date')(date, 'MMM dd HH:mm');
+                } else {
+                    return $filter('date')(date, 'MMM dd yyyy');
+                }
+            };
+        })
+        .filter('dataSize', function() {
+            var BYTES_PER_KB = Math.pow(2, 10);
+            var BYTES_PER_MB = Math.pow(2, 20);
+            var BYTES_PER_GB = Math.pow(2, 30);
+
+            return function(bytes) {
+                if (bytes == null || isNaN(bytes)) {
+                    return '';
+                } else if (bytes < BYTES_PER_KB) {
+                    return bytes.toFixed() + ' B';
+                } else if (bytes < BYTES_PER_MB) {
+                    return (bytes / BYTES_PER_KB).toFixed() + ' KB';
+                } else if (bytes < BYTES_PER_GB) {
+                    return (bytes / BYTES_PER_MB).toFixed() + ' MB';
+                } else {
+                    return (bytes / BYTES_PER_GB).toFixed(1) + ' GB';
+                }
+            };
         });
 
 })();
