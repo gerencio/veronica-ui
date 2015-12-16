5e456e8360285add6d86538fc401e0ffd478e4d9
Vagner Dutra <vagnerdutra@outlook.com>
2015-12-16 16:42:09 -0200

    incluindo jobs no banco de dados (mongodb)

--- admin-app/public/app/scripts/controllers/jobs/jobs.js
+++ admin-app/public/app/scripts/controllers/jobs/jobs.js
@@ -8,6 +8,57 @@
         function($scope, $http, $location) {
 
             $scope.jobs = {};
+            $scope.job_id = {};
+            $scope.jobFields = [
+                {
+                    key: 'name',
+                    type: 'input',
+                    templateOptions: {
+                    type: 'text',
+                        label: 'name'
+                    }
+                },
+                {
+                    key: 'description',
+                    type: 'input',
+                    templateOptions: {
+                    type: 'text',
+                        label: 'description'
+                    }
+                },
+                {
+                    key: 'command',
+                    type: 'input',
+                    templateOptions: {
+                    type: 'text',
+                        label: 'command'
+                    }
+                },
+                {
+                    key: 'owner',
+                    type: 'input',
+                    templateOptions: {
+                    type: 'text',
+                        label: 'owner'
+                    }
+                },
+                {
+                    key: 'ownerName',
+                    type: 'input',
+                    templateOptions: {
+                    type: 'text',
+                        label: 'ownerName'
+                    }
+                },
+                {
+                    key: 'schedule',
+                    type: 'input',
+                    templateOptions: {
+                    type: 'text',
+                        label: 'schedule'
+                    }
+                }
+            ];
 
             // lista de jobs (chronos)
             $scope.jobs_view = function () {
@@ -18,9 +69,6 @@
                     success(function(data) {
                         console.log(data);
                         $scope.jobs = data;
-
-
-
                     }).
                     error(function(err) {
                         console.log(err);
@@ -28,6 +76,35 @@
 
             };
 
+
+
+
+            // lista de jobs ID (chronos)
+            $scope.jobs_view_id = function (id_job) {
+                var k=0;
+                $scope.jobs.forEach(function(obj) {
+                    if(id_job == obj.name){
+                        $scope.job_id = $scope.jobs[k];
+                        $("#createJob").modal();
+                    }
+                    k++;
+                });
+            };
+
+
+
+            // Salvar job no BD
+            $scope.jobs_save = function () {
+                console.log($scope.job_id);
+                $http.post('/jobs', { job: $scope.job_id })
+                    .then(
+                        function(data){
+                            console.log(data);
+                        }, function(err){
+                            console.log(err)
+                        }
+                    );
+            };
 
 
         }]);
